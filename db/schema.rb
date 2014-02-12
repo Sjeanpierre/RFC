26b55c85acf251dd5a7a916ab4da630d2f4bdaac
stevenson.jean-pierre@sage.com <stevenson.jean-pierre@sage.com>
2014-02-12 04:24:31 -0500

    Added Comments to changes
    Fixes issue #62

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140104161857) do
+ActiveRecord::Schema.define(version: 20140210231333) do
 
   create_table "approvers", force: true do |t|
     t.integer  "user_id"
@@ -46,6 +46,23 @@ ActiveRecord::Schema.define(version: 20140104161857) do
     t.integer  "created_by"
     t.datetime "change_date"
   end
+
+  create_table "comments", force: true do |t|
+    t.integer  "commentable_id",   default: 0
+    t.string   "commentable_type"
+    t.string   "title"
+    t.text     "body"
+    t.string   "subject"
+    t.integer  "user_id",          default: 0, null: false
+    t.integer  "parent_id"
+    t.integer  "lft"
+    t.integer  "rgt"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["commentable_id", "commentable_type"], name: "index_comments_on_commentable_id_and_commentable_type", using: :btree
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
   create_table "events", force: true do |t|
     t.integer  "change_id"
