3bfd044f17a6ec1d7d8a72a2eea4ac253a231c48
Stevenson Jean-Pierre <stevenson.jean-pierre@sage.com>
2015-01-18 00:13:40 -0500

    working ui improvements

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,8 @@
 class SessionsController < ApplicationController
   skip_before_filter :require_login
-  def new; end
+  def new
+    redirect_to root_path if logged_in?
+  end
 
   def destroy
     logout!
